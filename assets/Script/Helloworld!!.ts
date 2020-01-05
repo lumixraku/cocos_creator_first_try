1bcc7e90fb753fcaf2a72d597ee07f2ce22946cc
luonan <luonan@bytedance.com>
2020-01-05 22:23:58 +0800

    first cocos !
    Change-Id: I013827d595ea8d884f5276c3466d04f896deef12

---
+++ assets/Script/Helloworld!!.ts
@@ -0,0 +1,20 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class Helloworld extends cc.Component {
+
+    @property(cc.Label)
+    label: cc.Label = null;
+
+    @property
+    text: string = 'hello';
+
+    start () {
+        // init logic
+        this.label.string = this.text;
+    }
+
+    update(dt){
+        console.log("update >>>>>>>><<<<<")
+    }
+}
---
+++ assets/Script/Player.ts
@@ -0,0 +1,202 @@
+// Learn TypeScript:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/typescript.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+
+@ccclass
+export default class NewScript extends cc.Component {
+
+    // 主角跳跃高度
+    @property
+    jumpHeight = 0;
+    // 主角跳跃持续时间
+    @property
+    jumpDuration = 0;
+    // 辅助形变动作时间
+    @property
+    squashDuration = 0;
+    // 最大移动速度
+    @property
+    maxMoveSpeed = 0;
+    // 加速度
+    @property
+    accel = 0;
+    // 跳跃音效资源
+    @property({
+        type: cc.AudioClip
+    })
+    jumpAudio = null;
+
+    // 加速度方向开关
+    accLeft = false;
+    accRight = false;
+
+    // 主角当前水平方向速度
+    xSpeed = 0;
+    // screen boundaries
+    minPosX = 0;
+    maxPosX = 0;
+
+    // 初始化跳跃动作
+    jumpAction = null;
+
+    // use this for initialization
+    onLoad() {
+        // this.enabled = false;
+
+        // compute screen boundaries
+        this.minPosX = -this.node.parent.width / 2;
+        this.maxPosX = this.node.parent.width / 2;
+
+        // 初始化键盘输入监听
+        this.setInputControl();
+
+        // 初始化跳跃动作
+        this.jumpAction = this.setJumpAction();
+        this.node.runAction(this.jumpAction);
+
+        // 加速度方向开关
+        this.accLeft = false;
+        this.accRight = false;
+        // 主角当前水平方向速度
+        this.xSpeed = 0;        
+
+    }
+    
+    // called every frame
+    update(dt) {
+        console.log(".........dt  update .........")
+        // 根据当前加速度方向每帧更新速度
+        if (this.accLeft) {
+            this.xSpeed -= this.accel * dt;
+            console.log()
+        } else if (this.accRight) {
+            this.xSpeed += this.accel * dt;
+        }
+        // 限制主角的速度不能超过最大值
+        if (Math.abs(this.xSpeed) > this.maxMoveSpeed) {
+            // if speed reach limit, use max speed with current direction
+            this.xSpeed = this.maxMoveSpeed * this.xSpeed / Math.abs(this.xSpeed);
+        }
+
+        // 根据当前速度更新主角的位置
+        this.node.x += this.xSpeed * dt;
+
+        // limit player position inside screen
+        if (this.node.x > this.node.parent.width / 2) {
+            this.node.x = this.node.parent.width / 2;
+            this.xSpeed = 0;
+        } else if (this.node.x < -this.node.parent.width / 2) {
+            this.node.x = -this.node.parent.width / 2;
+            this.xSpeed = 0;
+        }
+    }
+
+    setInputControl() {
+        //add keyboard input listener to jump, turnLeft and turnRight
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+        // touch input
+        this.node.parent.on(cc.Node.EventType.TOUCH_START, this.onTouchBegan, this);
+        this.node.parent.on(cc.Node.EventType.TOUCH_END, this.onTouchEnded, this);
+    }
+
+    onKeyDown(event) {
+        console.log("on key down evt", event)
+        switch (event.keyCode) {
+            case cc.macro.KEY.a:
+            case cc.macro.KEY.left:
+                this.accLeft = true;
+                this.accRight = false;
+                break;
+            case cc.macro.KEY.d:
+            case cc.macro.KEY.right:
+                this.accLeft = false;
+                this.accRight = true;
+                break;
+        }
+    }
+
+    onKeyUp(event) {
+        switch (event.keyCode) {
+            case cc.macro.KEY.a:
+            case cc.macro.KEY.left:
+                this.accLeft = false;
+                break;
+            case cc.macro.KEY.d:
+            case cc.macro.KEY.right:
+                this.accRight = false;
+                break;
+        }
+    }
+
+    onTouchBegan(event) {
+        var touchLoc = event.getLocation();
+        if (touchLoc.x >= cc.winSize.width / 2) {
+            this.accLeft = false;
+            this.accRight = true;
+        } else {
+            this.accLeft = true;
+            this.accRight = false;
+        }
+        // don't capture the event
+        return true;
+    }
+
+    onTouchEnded(event) {
+        this.accLeft = false;
+        this.accRight = false;
+    }
+
+    setJumpAction() {
+        console.log("......jump Action ......")
+
+        // 跳跃上升
+        var jumpUp = cc.moveBy(this.jumpDuration, cc.v2(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
+
+        
+
+        // 下落
+        var jumpDown = cc.moveBy(this.jumpDuration, cc.v2(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
+        // 形变
+        var squash = cc.scaleTo(this.squashDuration, 1, 0.6);
+        var stretch = cc.scaleTo(this.squashDuration, 1, 1.2);
+        var scaleBack = cc.scaleTo(this.squashDuration, 1, 1);
+        // 添加一个回调函数，用于在动作结束时调用我们定义的其他方法
+        var callback = cc.callFunc(this.playJumpSound, this);
+        // 不断重复，而且每次完成落地动作后调用回调来播放声音
+        return cc.repeatForever(cc.sequence(squash, stretch, jumpUp, scaleBack, jumpDown, callback));
+    }
+
+    playJumpSound() {
+        // 调用声音引擎播放声音
+        // cc.audioEngine.playEffect(this.jumpAudio, false);
+    }
+
+    getCenterPos() {
+        var centerPos = cc.v2(this.node.x, this.node.y + this.node.height / 2);
+        return centerPos;
+    }
+
+    startMoveAt(pos) {
+        this.enabled = true;
+        this.xSpeed = 0;
+        this.node.setPosition(pos);
+        this.node.runAction(this.setJumpAction());
+    }
+
+    stopMove() {
+        this.node.stopAllActions();
+    }
+
+
+}
+
